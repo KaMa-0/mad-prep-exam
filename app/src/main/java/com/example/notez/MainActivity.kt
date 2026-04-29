1ed955e8b814a33ef196db230be34df04e556d49
KaMa <maid.kanuric>
2026-04-29 17:43:39 +0200

    Initial commit - copy of master repo

---
+++ app/src/main/java/com/example/notez/MainActivity.kt
@@ -0,0 +1,59 @@
+/*
+ *
+ *  * Copyright 2025 Google LLC. All rights reserved.
+ *  *
+ *  * Licensed under the Apache License, Version 2.0 (the "License");
+ *  * you may not use this file except in compliance with the License.
+ *  * You may obtain a copy of the License at
+ *  *
+ *  *     http://www.apache.org/licenses/LICENSE-2.0
+ *  *
+ *  * Unless required by applicable law or agreed to in writing, software
+ *  * distributed under the License is distributed on an "AS IS" BASIS,
+ *  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  * See the License for the specific language governing permissions and
+ *  * limitations under the License.
+ *
+ */
+
+package com.example.notez
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import androidx.core.content.IntentCompat
+import com.example.notez.core.data.NoteType
+import com.example.notez.features.home.NotezApp
+import com.example.notez.core.ui.theme.NotezAppTheme
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+        val noteId = intent.getLongExtra(AppArgs.NOTE_ID_KEY, -1)
+        val noteType = IntentCompat.getParcelableExtra(
+            intent,
+            AppArgs.NOTE_TYPE_KEY,
+            NoteType::class.java
+        )
+
+        setContent {
+            NotezAppTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    NotezApp(noteId = noteId, noteType = noteType)
+                }
+            }
+        }
+    }
+}
